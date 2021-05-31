bddb2c0d85d5e39341a5bb5259b1ca8ab4275552
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-05-31 16:20:27 +0200

    Exercise

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
 
-a = [1.3, 3.4, 2.3, 9.8]
+x = [14.2, 16.5, 11.8, 15.3, 18.8, 22.5, 19.5]
+y = [220.00, 330.00, 190.00, 340.00, 410.00, 445.00, 415.00]
+plt.title("Soup sales in relation to temperature")
+plt.xlabel("Temperature (%)")
+plt.ylabel("Price in (R)")
+plt.scatter(x, y)
+
+"""a = [1.3, 3.4, 2.3, 9.8]
 b = [3.5, 4.9, 1.3, 2.2]
 c = [9.7, 1.5, 4.3, 0.9, 11.2]
 data1 = [a, b, c]
@@ -10,11 +17,6 @@ plt.xlabel("Colleges")
 plt.ylabel("Scores")
 plt.title("Boxplot for colleges")
 plt.subplot(122)
-plt.hist(data1, bins=5)
-
-"""x = [5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6]
-y = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
-
-plt.scatter(x, y)"""
+plt.hist(data1, bins=5)"""
 
 plt.show()
